d261cde3259af90951ef22d01644f60174cd31bc
John Brady <bradyjoh@grinnell.edu>
2013-09-13 17:02:41 -0500

    Added files made yesterday by Fiona

---
+++ src/edu/grinnell/csc207/byrnefio/hw3/Calculator.java
@@ -0,0 +1,24 @@
+package edu.grinnell.csc207.byrnefio.utils;
+
+import java.math.BigInteger;
+
+public class Calculator {
+
+	/* 
+	 * Reads a string containing a mathematical 
+	 * expression and evaluates it from left to right
+	 */
+	public BigInteger eval0 (String expression) {
+		return null;
+	} //eval0
+
+	/* 
+	 * Calculates the fewest number of coins of specific 
+	 * denominations needed to make change for the value
+	 * given
+	 */
+	public static int[] fewestCoins (int payment) {
+		return null;
+	} //fewestCoins
+
+} //Calculator
---
+++ src/edu/grinnell/csc207/byrnefio/hw3/StringUtils.java
@@ -0,0 +1,32 @@
+package edu.grinnell.csc207.byrnefio.utils;
+
+public class StringUtils {
+
+	/*
+	 * Splits a string into an array of substrings
+	 * separated by a particular character
+	 */
+	public String splitAt(String splitText, char splitChar) {
+		return null;
+	} //splitAt
+	
+	/*
+	 * Splits a string into an array of substrings
+	 * using a comma as the separator and 
+	 */
+	public String splitCSV(String splitText) {
+		return null;
+	} //splitCSV
+	
+	/*
+	 *  Converts a string from leet to plaintext
+	 */
+	public static String deLeet (String leet) {
+		return null;
+	} //deLeet
+	
+	public String nameGame (String name)  {
+		return null;
+	} //nameGame
+
+} //StringUtils
