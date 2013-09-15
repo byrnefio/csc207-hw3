8c21c0d277811e2e6c9526eacd10998b06e5062b
John Brady <bradyjoh@grinnell.edu>
2013-09-15 00:35:11 -0500

    deLeet finished, testing next. 1 hour of work
    Saw Tiffany's code using an enhanced for loop, and wanted to find a
    different way to do it. Is that allowed?

--- src/edu/grinnell/csc207/byrnefio/hw3/StringUtils.java
+++ src/edu/grinnell/csc207/byrnefio/hw3/StringUtils.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.byrnefio.hw3;
 import java.io.PrintWriter;
+
 public class StringUtils {
 
 	/*
@@ -76,70 +77,22 @@ public class StringUtils {
 	/*
 	 *  Converts a string from leet to plaintext
 	 */
-	public static String deLeet (String leet) {
-	    // l33t alphabet
-	    String a = "@";
-	    String b = "|3";
-	    String c = "(";
-	    String d = "|)";
-	    String e = "3";
-	    String f = "|=";
-	    String g = "9";
-	    String h = "#";
-	    String i = "|";
-	    String k = "\\<";
-	    String l = "1";
-	    String m = "/v\\";
-	    String n = "|\\|";
-	    String o = "0";
-	    String s = "5";
-	    String t = "+";
-	    
-	    //String[][] leetDict= [2][2];
-	   // leetDict [0][0] = {"a", "@"}; //how do you do 2d arrays I will figure this out later because I need to go 
-	    String temp = leet.toLowerCase();
-	    String output="";
-	    for (int j = 1; j <= leet.length(); j++) {
-		String interpret=temp.substring(j-1,j);
-		switch (interpret) {
-		case "a": output.concat("@");
-		break;
-		case "b": output.concat("|3");
-		break;
-		case "c": output.concat("(");
-		break;
-		case "d": output.concat("|)");
-		break;
-		case "e": output.concat("3");
-		break;
-		case "f": output.concat("|=");
-		break;
-		case "g": output.concat("9");
-		break;
-		case "h": output.concat("#");
-		break;
-		case "i": output.concat("|");
-		break;
-		case "k": output.concat("\\<");
-		break;
-		case "l": output.concat("1");
-		break;
-		case "m": output.concat("/v\\");
-		break;
-		case "n": output.concat("|\\|");
-		break;
-		case "o": output.concat("0");
-		break;
-		case "s": output.concat("5");
-		break;
-		case "t": output.concat("+");
-		break;
-		default: output.concat(interpret);
-		break;
-		}
-	
-		
-	    }
+	public static String deLeet (String leet){ //replaceAll didn't work, found solution here: http://stackoverflow.com/a/4105860
+	    String output = leet.replace("|\\|","n");
+	    output = output.replace("|3", "b");
+	    output = output.replace("/v\\", "m");
+	    output = output.replace("\\<", "k");
+	    output = output.replace("|)", "d");
+	    output = output.replace("|=", "f");
+	    output = output.replace("+", "t");
+	    output = output.replace("@", "a");
+	    output = output.replace("e", "3");
+	    output = output.replace("|", "l");
+	    output = output.replace("0", "o");
+	    output = output.replace("1", "i");
+	    output = output.replace("#", "h");
+	    output = output.replace("9", "g");
+	    output = output.replace("(", "c");   
 	    return output;	
 	} //deLeet
 
@@ -193,6 +146,10 @@ public class StringUtils {
 	    nameGame("Shirley");
 	    nameGame("Lincoln");
 	    nameGame("Olivia");
+	    
+	    //testing deLeet
+	    System.out.println(deLeet("3@+ |3@|\\|@|\\|@"));
+	    
 	} // main
 	
 
